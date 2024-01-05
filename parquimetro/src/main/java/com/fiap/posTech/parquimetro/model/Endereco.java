425a36654b960f4e7b3bb29d9e8c2ecbac3cdec6
ricardo peres <ricardo.peres2013@gmail.com>
2024-01-05 20:11:29 -0300

    Inclusão do model pessoa e endereço

--- parquimetro/src/main/java/com/fiap/posTech/parquimetro/model/Endereco.java
+++ parquimetro/src/main/java/com/fiap/posTech/parquimetro/model/Endereco.java
@@ -6,20 +6,20 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.Year;
-
-@Document("veiculo")
+@Document("endereco")
 @Data
 @NoArgsConstructor
-public class Veiculo {
-
+public class Endereco {
     @Id
-    private String id;
-
-    private String modelo;
-    private String cor;
-    private Year anoFabrica;
-    private Year anoModelo;
+    private String codigo;
+    private Integer cep;
+    private String tipoLogradouro; //Trabalho, Home, etc
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
 
     @Version
     private Long version;
---
+++ parquimetro/src/main/java/com/fiap/posTech/parquimetro/model/Pessoa.java
@@ -0,0 +1,30 @@
+package com.fiap.posTech.parquimetro.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document("pessoa")
+@Data
+@NoArgsConstructor
+public class Pessoa {
+    @Id
+    private String codigo;
+    private Integer cpf;
+    private String rg;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String email;
+    private String celular;
+
+    @DBRef
+    private Endereco endereco;
+
+    @Version
+    private Long version;
+}
