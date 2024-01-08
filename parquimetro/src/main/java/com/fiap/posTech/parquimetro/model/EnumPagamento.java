8c6799cb793559b8181421d46ac3ed66b1dcaaae
Warley Araujo <warley1955@gmail.com>
2024-01-08 15:31:38 -0300

    adicionando o enum de pagamento

---
+++ parquimetro/src/main/java/com/fiap/posTech/parquimetro/model/EnumPagamento.java
@@ -0,0 +1,24 @@
+package com.fiap.posTech.parquimetro.model;
+public enum EnumPagamento {
+    DINHEIRO("1", "DINHEIRO"),
+    DEBITO("2", "DEBITO"),
+    CREDITO("3", "CREDITO"),
+    PIX("4", "PIX");
+
+    private String description;
+    private Long value;
+
+    EnumPagamento(String description, Long value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Long getValue() {
+        return this.value;
+    }
+}
+
